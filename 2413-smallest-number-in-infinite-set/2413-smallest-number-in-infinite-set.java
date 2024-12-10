148c50ae058d1ac3f2cc658679c652ebfeb231d9
Vivek Chavda <115214436+Vivekchavda1374@users.noreply.github.com>
2024-12-10 14:52:01 +0530

    Time: 9 ms (88.72%), Space: 45.7 MB (24.23%) - LeetHub

---
+++ 2413-smallest-number-in-infinite-set/2413-smallest-number-in-infinite-set.java
@@ -0,0 +1,32 @@
+class SmallestInfiniteSet {
+    int head;
+    boolean[] visited;
+    SmallestInfiniteSet() {
+        visited = new boolean[1001];
+        Arrays.fill(visited, true);
+        head = 1;
+    }
+    
+    int popSmallest() {
+        int ans = head;
+        visited[ans] = false;
+        for(int i = head + 1; i <= 1000; i++){
+            if(visited[i]){
+                head = i;
+                break;
+            }
+        }
+        return ans;
+    }
+    
+    void addBack(int num) {
+        if(visited[num])
+            return;
+        
+        visited[num] = true;
+        
+        if(num < head){
+            head = num;
+        }
+    }
+}
